2834faf47e3e68c2a51adfe9491e15b9ebe10f86
Pernod70 <ngbarnes@hotmail.com>
2014-02-15 22:00:09 +0000

    Initial commit.

---
+++ src/make.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+chown -R root:root desktop etc phpPgAdmin ui
+chmod -R 444 *
+chmod -R a+X *
+chmod -R u+rw *
+tar -czf package.tgz -X exclude.txt desktop etc phpPgAdmin ui
+
+chown -R root:root scripts CHANGELOG LICENSE INFO package.tgz PACKAGE_ICON.PNG
+chmod -R 755 *
+tar -cvf ../phpPgAdmin-5.0.4-002.spk -X exclude.txt scripts CHANGELOG LICENSE INFO package.tgz PACKAGE_ICON.PNG
+
+exit 0
